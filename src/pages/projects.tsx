6e94a0f66e364af3bb15e347162ff5f39ab19f40
GunnarDraft <gunnardraft@live.com.mx>
2023-07-07 02:31:00 -0600

    matrix effect

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -532,9 +532,7 @@ export default function Projects() {
             <pointLight position={[10, 10, 10]} intensity={1.5} />
             <Suspense fallback={null}>
                 <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}>
-                    <Model />
-                    <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
-
+                    <Model /> 
                 </group> 
             </Suspense>
             <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
